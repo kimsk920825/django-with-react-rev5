1859feebde039fd1acf967593824c783d2726072
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-02-18 21:50:03 +0900

    feat: EP03 -react-router-dom을 활요한 라우팅 처리

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import AppLayout from "components/AppLayout";
+import Home from "./Home";
+import About from "./About";
+import AccountRoutes from "./accounts/index";
+
 function Root() {
-  return <div> 최상위 컴포넌트 </div>;
+  return (
+    <AppLayout>
+      최상위 컴포넌트
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route path="/accounts" component={AccountRoutes} />
+    </AppLayout>
+  );
 }
 
 export default Root;
