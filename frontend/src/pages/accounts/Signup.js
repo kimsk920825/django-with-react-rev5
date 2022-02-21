d0517f81ee886ff766c3680c7d35ac4348113a0a
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-02-21 20:20:22 +0900

    feat: EP05 -회원가입 API 만들기 EP06 -리액트 기본 기능으로 회원가입 폼 만들기

---
+++ frontend/src/pages/accounts/Signup.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { Alert, message } from "antd";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+export default function Signup() {
+  const history = useHistory();
+  const [inputs, setInputs] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [formDisabled, setFormDisabled] = useState(true);
+  //   const [username, setUsername] = useState("");
+  //   const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault({});
+    setLoading(true);
+    setErrors({});
+
+    Axios.post("http://localhost:8000/accounts/signup/", inputs)
+      .then((response) => {
+        history.push("/accounts/login");
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+        if (error.response) {
+          setErrors({
+            username: (error.response.data.username || []).join(" "),
+            password: (error.response.data.password || []).join(" "),
+          });
+        }
+      });
+  };
+
+  useEffect(() => {
+    console.log("changed inputs", inputs);
+  }, [inputs]);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    const isEnabled = Object.values(inputs).every((s) => s.length > 0);
+    setFormDisabled(!isEnabled);
+  }, [inputs]);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="text" name="username" onChange={onChange} />
+          {errors.username && <Alert type="error" message={errors.username} />}
+        </div>
+        <div>
+          <input type="password" name="password" onChange={onChange} />
+          {errors.password && <Alert type="error" message={errors.password} />}
+        </div>
+
+        <input type="submit" value="회원가입" disabled={formDisabled} />
+      </form>
+    </div>
+  );
+}
